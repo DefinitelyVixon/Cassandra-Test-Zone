e36188fa323a211ca2ba046adab65a0582d7d044
DefinitelyVixon <egealtiok@hotmail.com>
2022-07-18 14:29:54 +0300

    Data parsing and better querying implementation.

--- SemesterGrades.py
+++ SemesterGrades.py
@@ -1,53 +1,76 @@
 from cassandra.cluster import Cluster
-from cassandra.query import dict_factory
-import csv
-import openpyxl
+import json
+import numpy as np
 import pandas as pd
 
 sheet_names = ['21fall', '20spring', '20fall', '19spring', '19fall']
-# pd.set_option('display.max_rows', None)
+pd.set_option('display.max_rows', None)
 
 
-class CassandraSession:
+class QueryManager:
 
-    def __init__(self):
-        self.cluster = None
-        self.session = None
-        self.prepared_queries = None
-
-    def connect(self, keyspace):
+    def __init__(self, keyspace):
         self.cluster = Cluster()
         self.session = self.cluster.connect(keyspace)
-        self.session.row_factory = dict_factory
-        self.prepared_queries = {
+        self.session.row_factory = QueryManager.pandas_factory
+        self.session.default_fetch_size = None
+        self.queries = {
             'insert': self.session.prepare(
                 "INSERT INTO gpa_table (student_id, gpa, semester, reg_year, dep_code, student_name) "
                 "VALUES (?, ?, ?, ?, ?, ?);"
             ),
             'select': self.session.prepare(
-                "SELECT * FROM gpa_table WHERE dep_code=? AND semester=?"
-            )
+                "SELECT * FROM gpa_table WHERE dep_code=? AND semester=?;"
+            ),
+            'truncate': self.session.prepare(
+                "TRUNCATE gpa_table;"),
+            'update_std_name': self.session.prepare(
+                "UPDATE gpa_table SET student_name=? WHERE dep_code=? AND semester=? AND student_id=?;")
         }
-        print("Session is ready.")
+        self.contents = None
+        with open('data.json', 'r+', encoding='latin1') as io_file:
+            self.contents = json.load(io_file)
+            self.dep_code_to_name = self.contents['dep_code_to_name']
+            self.dep_name_to_code = self.contents['dep_name_to_code']
 
-    def select_query(self, dep_code, semester):
-        result = self.session.execute(self.prepared_queries['select'], (dep_code, semester))
+        print('Session is ready.')
 
-        for row in result.all():
-            print('%(student_name)20s %(student_id)11s %(gpa).2f %(semester)8s %(dep_code)4s %(reg_year)4d' % row)
+    def select(self, dep_code, semester):
+        result_set = self.session.execute(self.queries['select'], (dep_code, semester))
+        return result_set._current_rows
 
-    def insert_query(self, student_df):
+    def insert(self, student_df=None):
+        if student_df is None:
+            student_df = QueryManager.read_excel()
         try:
             for student in student_df.loc:
-                self.session.execute(self.prepared_queries['insert'], (student.tolist()))
+                self.session.execute(self.queries['insert'], (student.tolist()))
         except KeyError:
             pass
         print(f'Inserted {len(student_df.index)} rows.')
 
+    def update_student_names(self):
+        for student_id, student_info in self.contents['student_infos'].items():
+            for semester in sheet_names:
+                self.session.execute(self.queries['update_std_name'],
+                                     [student_info['name'],
+                                      student_info['dep_code'],
+                                      semester,
+                                      int(student_id)])
 
-if __name__ == '__main__':
+    def truncate(self):
+        self.session.execute(self.queries['truncate'], [])
 
+    @staticmethod
+    def pandas_factory(colnames, rows):
+        if len(rows) == 1:
+            return rows[0][0]
+        return pd.DataFrame(rows, columns=colnames)
+
+    @staticmethod
     def read_excel():
+        import openpyxl
+
         gpa_df = pd.DataFrame(columns=['student_id', 'gpa', 'semester'])
         for sheet_name in sheet_names:
             semester_df = pd.read_excel(open('semester_list.xlsx', 'rb'),
@@ -63,19 +86,51 @@ if __name__ == '__main__':
             student_id = str(gpa_df.loc[i, 'student_id'])
             gpa_df.loc[i, 'reg_year'] = int(student_id[:4])
             gpa_df.loc[i, 'dep_code'] = student_id[4:8]
-
         return gpa_df
 
-    # print('Bilgisayar M\\u00fchendisli\\u011fi'.encode('latin1').decode('unicode_escape'))
+    def parse_student_info(self, filename):
+        student_id_info = {}
+        dep_code_to_name = {}
 
-    cs = CassandraSession()
-    cs.connect('test_keyspace')
+        for std_id, std_info in list(zip(self.contents['student_ids'], self.contents['raw_infos'])):
+            std_info = std_info.encode('latin1').decode('unicode_escape')
+            s_list = std_info.split()
 
-    student_gpa_df = read_excel()
-    student_gpa_df = student_gpa_df.sort_values(by='gpa', ascending=False, ignore_index=True)
+            for i in range(len(s_list)):
+                if not s_list[i].isupper():
+                    std_name = ' '.join(s_list[:i])
+                    dep_name = ' '.join(s_list[i:])
+                    dep_code = std_id[4:8]
 
-    # cs.insert_query(student_gpa_df.loc[0:1000, :])
-    cs.insert_query(student_gpa_df)
+                    student_id_info[std_id] = {'name': std_name,
+                                               'dep_code': dep_code}
+                    dep_code_to_name[dep_code] = dep_name
+                    break
 
-    for sem in sheet_names:
-        cs.select_query('0602', sem)
+        with open(filename, 'w', encoding='utf8') as json_file:
+            self.contents['student_infos'] = student_id_info
+            self.contents['dep_code_to_name'] = dep_code_to_name
+
+            dep_name_to_code = {}
+            for k, v in dep_code_to_name.items():
+                dep_name_to_code[v] = k
+
+            self.contents['dep_name_to_code'] = dep_name_to_code
+            json.dump(self.contents, json_file)
+
+
+if __name__ == '__main__':
+
+    qm = QueryManager('test_keyspace')
+
+    semesters = sheet_names
+    selected_df = pd.DataFrame(columns=['student_id', 'gpa', 'student_name', 'semester', 'reg_year', 'dep_code'])
+
+    for sem in semesters:
+        semester_grades = qm.select('0602', sem)
+        selected_df = pd.concat([selected_df, semester_grades], ignore_index=True)
+
+    selected_df = selected_df.sort_values(by='gpa', ascending=False, ignore_index=True).dropna()
+    selected_df['reg_year'] = selected_df['reg_year'].astype(np.int32)
+
+    print(selected_df)
